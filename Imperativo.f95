05e61a018fbc77f021fbc0ea11c8a9eea9e13144
unsis <jared0192galicia@gmail.com>
2023-03-10 08:55:03 -0600

    subir archivos

---
+++ Imperativo.f95
@@ -0,0 +1,20 @@
+! 
+! Welcome to GDB Online.
+! GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+! C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+! Code, Compile, Run and Debug online from anywhere in world.
+! 
+! 
+Program SumaNumeros
+
+implicit none
+
+integer num1, num2, suma
+
+write(*,*) 'Ingrese el primer numero: '
+read(*,*)num1
+write(*,*) 'Ingrese el segundo numero: '
+read(*,*)num2
+suma = num1 + num2
+write(*,*) 'El resultado es:', suma
+End Program SumaNumeros
---
+++ README.md
@@ -0,0 +1,8 @@
+# Paradigmas de programacion
+### Realizar un programa que sume dos numeros en distintos paradigmas
+
+* Imperativo
+* Funcional
+* Lógico
+* Orientado a eventos
+* Otro (Estructurado)
